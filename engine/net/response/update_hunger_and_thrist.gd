c55900764bc2211e127d44ca18084a75732ac05e
brian-christopher <brian-christopher1995@outlook.com>
2024-09-05 23:28:28 -0300

    Update update_hunger_and_thrist.gd

--- engine/net/response/update_hunger_and_thrist.gd
+++ engine/net/response/update_hunger_and_thrist.gd
@@ -5,12 +5,10 @@ var thirst:int
 var hunger:int
 
 static func unpack(stream: StreamPeerBuffer) -> UpdateHungerAndThirstResponse:
-	var p = UpdateHungerAndThirstResponse.new()
-	
+	var p = UpdateHungerAndThirstResponse.new() 
+	stream.get_u8()
 	p.thirst = stream.get_u8()
-	stream.get_u8()
 	
-	p.hunger = stream.get_u8()
 	stream.get_u8()
-	
+	p.hunger =stream.get_u8() 
 	return p
